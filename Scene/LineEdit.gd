2a62257c0765752aa06b1bbbf0e2e0458e6ef349
Bennett Upfield <bjupfield@gmail.com>
2023-10-25 21:01:42 -0500

    very lazy sick... but found out how to get any input to assign for input mapping

---
+++ Scene/LineEdit.gd
@@ -0,0 +1,7 @@
+extends LineEdit
+
+
+
+func _on_text_changed(new_text):
+	self.text = "b"
+	pass # Replace with function body.
--- Scripts/Quitter.gd
+++ Scripts/Quitter.gd
@@ -1,7 +1,12 @@
 extends Button
 
 
-
+func _input(event):
+	self.text = event.as_text()
+	pass
+	
+func _input_Mine(event):
+	pass
 
 func _on_button_down():
 	get_tree().quit(0)
