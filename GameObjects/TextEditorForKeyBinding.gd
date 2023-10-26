8ede8da1040ba4dafe2e5fce78e66f0bcf353e2e
Bennett Upfield <bjupfield@gmail.com>
2023-10-26 00:07:11 -0500

    okay actually last commit for tonight

--- GameObjects/TextEditorForKeyBinding.gd
+++ GameObjects/TextEditorForKeyBinding.gd
@@ -1,34 +1,27 @@
 extends RichTextLabel
 
+var eventName: String = ""
 var assigning: bool = false
 var upButtonAmount: int = 0
 # Called when the node enters the scene tree for the first time.
 func _input(event):
-	if(assigning and str(event.get_class()) != "InputEventMouseMotion"):
-		if(event is InputEventMouseButton):
-			if(event.button_index == MOUSE_BUTTON_LEFT):
-				if(!event.pressed):
-					if(upButtonAmount > 0):
-						self.text = str(upButtonAmount)
-						assigning = false
-					upButtonAmount += 1
-			else:
-				self.text = event.as_text()
-				assigning = false
-				upButtonAmount = 0
-		else:
-			self.text = event.as_text()
-			assigning = false
+	if(assigning and str(event.get_class()) != "InputEventMouseMotion" and (event.as_text() != "Left Mouse Button" or (event is InputEventMouseButton and event.pressed and upButtonAmount > 0))):
+		assigning = false
+		upButtonAmount = 0
+		self.text = "|" + event.as_text() + "|"
 	else:
-		if event is InputEventMouseButton && event.button_index == MOUSE_BUTTON_LEFT:
-			var posY: int = self.position.y
-			var posX: int = self.position.x
-			var sizeY: int = self.size.y
-			var sizeX: int = self.size.x
-			var hitX: int = event.position.x
-			var hitY: int = event.position.y
-			if(hitX > posX && hitX < (posX + sizeX) && hitY > posY && hitY < (posY + sizeY)):
-				self.text = "EnterInput"
-				assigning = true
-				upButtonAmount = 0
+		if event is InputEventMouseButton:
+			if event.button_index == MOUSE_BUTTON_LEFT and !assigning:
+				var posY: int = self.position.y
+				var posX: int = self.position.x
+				var sizeY: int = self.size.y
+				var sizeX: int = self.size.x
+				var hitX: int = event.position.x
+				var hitY: int = event.position.y
+				if(hitX > posX && hitX < (posX + sizeX) && hitY > posY && hitY < (posY + sizeY)):
+					self.text = "EnterInput"
+					assigning = true
+					upButtonAmount = 0
+			elif event.button_index == MOUSE_BUTTON_LEFT and assigning and !event.pressed:
+				upButtonAmount += 1;
 	pass
